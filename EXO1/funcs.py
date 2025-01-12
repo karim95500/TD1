6f01f6bc190660552237c2bcf8681e73d1e5ff9b
votre_login <votre_mail>
2025-01-12 20:24:55 +0100

    Ajout du dossier EXO1

--- EXO1/funcs.py
+++ EXO1/funcs.py
@@ -1,3 +1,17 @@
 def top_three(values):
     return sorted(values, reverse=True)[:3]
+ 
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def is_arithmetic_sequence(lst):
+    if len(lst) < 2:
+        return True
+    diff = lst[1] - lst[0]
+    return all(lst[i] - lst[i - 1] == diff for i in range(2, len(lst)))
     
--- EXO1/tests_funcs.py
+++ EXO1/tests_funcs.py
@@ -6,3 +6,17 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.top_three([3, 1, 4, 1, 5, 9]), [9, 5, 4])
         self.assertEqual(funcs.top_three([1, 2]), [2, 1])
         self.assertEqual(funcs.top_three([]), [])
+    def test_is_prime(self):
+        self.assertTrue(funcs.is_prime(2))
+        self.assertTrue(funcs.is_prime(13))
+        self.assertFalse(funcs.is_prime(4))
+        self.assertFalse(funcs.is_prime(1))
+        self.assertFalse(funcs.is_prime(-3))
+    def test_is_arithmetic_sequence(self):
+        self.assertTrue(funcs.is_arithmetic_sequence([1, 3, 5, 7])) 
+        self.assertFalse(funcs.is_arithmetic_sequence([1, 2, 4, 8]))  
+        self.assertTrue(funcs.is_arithmetic_sequence([10, 10, 10]))  
+        self.assertTrue(funcs.is_arithmetic_sequence([])) 
+        self.assertTrue(funcs.is_arithmetic_sequence([5])) 
+
+    
