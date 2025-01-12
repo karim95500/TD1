bf7986e7dbfb02e5f7c42124fc90cfd8ff0bb683
votre_login <votre_mail>
2025-01-12 20:50:30 +0100

    LIFO and FIFO Final version with test

--- EXO2/funcs2.py
+++ EXO2/funcs2.py
@@ -1,35 +1,40 @@
 class FIFOQueue:
     def __init__(self):
-        """Initialise la file."""
-        pass
+        self.queue = []  
 
     def enqueue(self, value):
         """Ajoute un élément à la fin de la file."""
-        pass
+        self.queue += [value]  
 
     def dequeue(self):
         """Retire et retourne l'élément au début de la file."""
-        pass
+        if not self.queue:
+            raise IndexError("Queue is empty")
+        value = self.queue[0]  
+        self.queue = self.queue[1:]  
+        return value
 
     def is_empty(self):
         """Vérifie si la file est vide."""
-        pass
-
+        return len(self.queue) == 0
 
 class LIFOQueue:
     def __init__(self):
-        """Initialise la pile."""
-        pass
+        self.stack = []  
 
     def push(self, value):
         """Ajoute un élément au sommet de la pile."""
-        pass
+        self.stack += [value]  
 
     def pop(self):
         """Retire et retourne l'élément au sommet de la pile."""
-        pass
+        if not self.stack:
+            raise IndexError("Stack is empty")
+        value = self.stack[-1]  
+        self.stack = self.stack[:-1]  
+        return value
 
     def is_empty(self):
         """Vérifie si la pile est vide."""
-        pass
+        return len(self.stack) == 0
 
